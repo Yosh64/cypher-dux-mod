54e2d0e4db0cc06a80c7b338a7e8c85639601e0e
Yosh64 <65314603+Yosh64@users.noreply.github.com>
2026-06-09 23:19:56 +0200

    Buffed Armors

--- src/main/java/com/yosh/cyphdux/armor/ModArmorMaterials.java
+++ src/main/java/com/yosh/cyphdux/armor/ModArmorMaterials.java
@@ -18,13 +18,13 @@ public class ModArmorMaterials {
         return Registry.registerReference(Registries.ARMOR_MATERIAL, CypherDuxMod.of(id),material.get());
     }
 
-    public static final int ROSE_GOLD_DURABILITY_MULTIPLIER = 22;
+    public static final int ROSE_GOLD_DURABILITY_MULTIPLIER = 30;
     public static final RegistryEntry<ArmorMaterial> ROSE_GOLD = registerArmorMaterial("rose_gold",
             ()->new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map ->{
-                map.put(ArmorItem.Type.BOOTS,2);
+                map.put(ArmorItem.Type.BOOTS,3);
                 map.put(ArmorItem.Type.LEGGINGS,5);
                 map.put(ArmorItem.Type.CHESTPLATE,6);
-                map.put(ArmorItem.Type.HELMET,2);
+                map.put(ArmorItem.Type.HELMET,3);
                 map.put(ArmorItem.Type.BODY,10);
             }),17,SoundEvents.ITEM_ARMOR_EQUIP_GOLD,()->Ingredient.ofItems(Items.GOLD_INGOT),
                     List.of(new ArmorMaterial.Layer(CypherDuxMod.of("rose_gold"))),1.0F,0));
@@ -62,11 +62,11 @@ public class ModArmorMaterials {
     public static final int WARDEN_DURABILITY_MULTIPLIER = 40;
     public static final RegistryEntry<ArmorMaterial> WARDEN = registerArmorMaterial("warden",
             ()->new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map ->{
-                map.put(ArmorItem.Type.BOOTS,4);
-                map.put(ArmorItem.Type.LEGGINGS,7);
-                map.put(ArmorItem.Type.CHESTPLATE,9);
-                map.put(ArmorItem.Type.HELMET,4);
+                map.put(ArmorItem.Type.BOOTS,5);
+                map.put(ArmorItem.Type.LEGGINGS,8);
+                map.put(ArmorItem.Type.CHESTPLATE,10);
+                map.put(ArmorItem.Type.HELMET,5);
                 map.put(ArmorItem.Type.BODY,12);
             }),18,SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE,()->Ingredient.ofItems(Items.ECHO_SHARD),
-                    List.of(new ArmorMaterial.Layer(CypherDuxMod.of("warden"))),4.0F,0.1F));
+                    List.of(new ArmorMaterial.Layer(CypherDuxMod.of("warden"))),4.0F,0.2F));
 }
